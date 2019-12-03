d26930083d163fab9bf22be7004b880e4df11c48
Caio Vinicius Alves Ferreira <caioviniciusaferreira@gmail.com>
2019-12-03 13:46:06 -0300

    Desafio-modulo3
    Falta renderizar a pagina ao selecionar a receita

--- Modulo03/Desafio/public/scripts.js
+++ Modulo03/Desafio/public/scripts.js
@@ -1,23 +1,9 @@
-const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
-// adiciona a funcionalidade ao clicar no elemento card para abrir o modal
+
 for (let card of cards){
     card.addEventListener("click", function(){
-        const IdImagem = card.getAttribute("id")
-        const name = card.getAttribute('h3')
-        const author = card.getAttribute('h4')
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("img").src=`/assets/${IdImagem}.png`
-        modalOverlay.querySelector("h3").innerHTML = card.querySelector('h3').textContent
-        modalOverlay.querySelector("h4").innerHTML = card.querySelector('h4').textContent
-
-
+        const receitaID = card.getAttribute("id")
+       window.location.href=`/recipe/${receitaID}`
     })
 }
-
-//adiciona a funcionalidade para fechar o Modal
-document.querySelector('.modal-close').addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-
-})
--- Modulo03/Desafio/server.js
+++ Modulo03/Desafio/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const recipes = require('/data')
+const recipes = require('./data')
 
 const server = express()
 
@@ -21,12 +21,27 @@ server.get("/", function(req, res){
 })
 
 server.get("/receitas", function (req, res){
-    return res.render("receitas")
+    return res.render("receitas", {items: recipes} )
 })
 
 server.get("/sobre", function (req, res){
     return res.render("sobre")
 })
+
+server.get("/recipe/:index", function (req,res){
+    
+    const recipeIndex= req.params.index;
+    const recipe= [recipeIndex]
+
+
+    if(!recipe){
+        return res.send("Recipe not found!")
+    }
+
+
+    return res.render("recipe", { items: recipe })
+})
+
 server.listen(5000, function(){
     console.log('server is running');   
 })
