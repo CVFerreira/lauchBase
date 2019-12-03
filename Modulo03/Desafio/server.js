2ffbfeece389bbee1e9e1649d1b8beab3dbd71e0
Caio Vinicius Alves Ferreira <caioviniciusaferreira@gmail.com>
2019-12-02 21:03:15 -0300

    Falta terminar o Desafio

---
+++ Modulo03/Desafio/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const recipes = require('/data')
+
+const server = express()
+
+server.use(express.static('public'))
+server.use(express.static('assets'))
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res){  
+    return res.render("index", {items: recipes})    
+})
+
+server.get("/receitas", function (req, res){
+    return res.render("receitas")
+})
+
+server.get("/sobre", function (req, res){
+    return res.render("sobre")
+})
+server.listen(5000, function(){
+    console.log('server is running');   
+})
