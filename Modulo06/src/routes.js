5791578645576b8b19f7f403aee1acb56e2fc96e
Caio Ferreira <caioviniciusaferreira@gmail.com>
2020-08-04 20:49:32 -0300

    inicio da codificação do home

--- Modulo06/src/routes.js
+++ Modulo06/src/routes.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
+
 const ProductController = require('./app/controllers/ProductController');
+const HomeController = require('./app/controllers/HomeController');
+const SearchController = require('./app/controllers/SearchController');
+
+
 const Product = require('./app/models/Product');
 
+routes.get('/', HomeController.index);
 
-routes.get('/', function(req, res){
-    return res.render("layout.njk")
-});
+routes.get('/products/search', SearchController.index)
 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
@@ -16,7 +20,6 @@ routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products', multer.array("photos", 6), ProductController.post)
 routes.put('/products', multer.array("photos", 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
-
 
 // Alias
 routes.get('/ads/create', function(req, res){
